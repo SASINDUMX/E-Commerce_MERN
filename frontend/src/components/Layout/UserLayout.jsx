ed762f777c7bff713924526d386daed98e1a0406
Sasindu Shalika <MXsasindushalika001@gmail.com>
2026-02-19 16:16:56 +0530

    Create UserLayout.jsx

---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,9 @@
+import Header from '../Common/Header'
+const UserLayout = () => {
+    return <>{/* Header */}
+        <Header />
+        {/* Footer */}
+        {/* Main content */}
+    </>
+};
+export default UserLayout;
