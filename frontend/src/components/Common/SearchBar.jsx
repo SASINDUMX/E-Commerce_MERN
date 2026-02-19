2caf9f7a1a0ae6f6d88b114498c4743b15684149
Sasindu Shalika <MXsasindushalika001@gmail.com>
2026-02-19 16:48:07 +0530

    Create SearchBar.jsx

---
+++ frontend/src/components/Common/SearchBar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { HiMagnifyingGlass, HiMiniXMark } from "react-icons/hi2";
+export const Searchbar = () => {
+    const[searchTerm,setSearchTerm]=useState("");
+    const[isOpen,setIsOpen]=useState(false);
+    const handleSearchToggle=()=>{
+        setIsOpen(!isOpen);
+
+    };
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        console.log("Search Tearm:",searchTerm);
+        setIsOpen(false)
+    };
+  return (
+    <div
+      className={`flex w-full items-center justify-center transition-all duration-300 ${
+        isOpen ? 'absolute left-0 top-0 z-50 h-24 w-full bg-white' : 'w-auto'
+      }`}
+    >
+        {isOpen?(
+    <form onSubmit={handleSearch} className="relative flex items-center justify-center w-full">
+        <div className="relative w-1/2">
+        <input type="text" placeholder="Search" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}
+        className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full
+        placeholder:text-gray-700"/>
+        {/* search icon */}
+        <button type="submit" className="absolute right-2 top-1/2 transform -translate-y-1/2 
+        text-gray-600 hover:text-gray-800">
+            <HiMagnifyingGlass className='h-6 w-6'/>
+        </button>
+
+        </div>
+        {/*close button*/}
+        <button type="button" onClick={handleSearchToggle}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800">
+            <HiMiniXMark className='h-6 w-6'/>
+        </button>
+
+    </form>):(
+        <button onClick={handleSearchToggle}>
+            <HiMagnifyingGlass className="h-6 w-6"/>
+        </button>
+        )
+    }
+    </div>)
+};
