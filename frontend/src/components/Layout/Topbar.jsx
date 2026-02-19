e4dac7326cc4c9ae745a6f0124322e028e991a87
Sasindu Shalika <MXsasindushalika001@gmail.com>
2026-02-19 16:16:26 +0530

    Create Topbar.jsx

---
+++ frontend/src/components/Layout/Topbar.jsx
@@ -0,0 +1,38 @@
+import { TbBrandMeta } from 'react-icons/tb'
+import { IoLogoInstagram } from 'react-icons/io'
+import { RiTwitterXLine } from 'react-icons/ri'
+
+const TopBar = () => {
+  return (
+    <div className="bg-[#ea2e0e] text-white">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="hidden md:flex items-center space-x-4">
+          <a href="#" className="hover:text-gray-300">
+            <TbBrandMeta className="h-5 w-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <IoLogoInstagram className="h-5 w-5" />
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            <RiTwitterXLine className="h-4 w-4" />
+          </a>
+        </div>
+
+        {/* Center: Message */}
+        <div className="text-sm text-center flex-grow">
+          <span>your shopping partner</span>
+        </div>
+
+        {/* Right: Contact Number */}
+        <div className="text-sm hidden md:block">
+          <a href="tel:+919876543210" className="hover:text-gray-300">
+            +91 9876543210
+          </a>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default TopBar
